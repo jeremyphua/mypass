0935c98713b67e7123a96630d2f87b4dce0b0d14
Jeremy Phua <jeremyphuachengtoon@gmail.com>
2022-06-18 18:04:14 +0800

    implement show command (#3)
    * Initialise command
    
    * Revert "Initialise command"
    
    This reverts commit 5413a74291611cb51ee80a72212be730521573c8.
    
    * implement method to get retrieve siteinfo from sites.json
    
    * Implement method to validate and retrieve master private key
    
    * implement show command
    
    * implement show to include username

--- pc/pc.go
+++ pc/pc.go
@@ -2,8 +2,13 @@ package pc
 
 import (
 	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 
 	"github.com/alexedwards/argon2id"
+	"github.com/jeremyphua/mypass/io"
 	"golang.org/x/crypto/nacl/box"
 	"golang.org/x/crypto/nacl/secretbox"
 )
@@ -24,6 +29,8 @@ func Argon2id(password string) (key []byte, err error) {
 	return
 }
 
+// Wrapper around secretbox.Seal
+// Convert key byte slice to 32-bytes
 // Randomly generate a nonce to eliminate risk of reusing nonce
 func SecretboxSeal(key []byte, message []byte) ([]byte, error) {
 	var nonce [24]byte
@@ -35,6 +42,17 @@ func SecretboxSeal(key []byte, message []byte) ([]byte, error) {
 	return secretbox.Seal(nonce[:], message, &nonce, &keyArr), nil
 }
 
+// Wrapper around secretbox.Open
+// Convert key byte slice to 32-bytes
+func SecretboxOpen(key []byte, encrypted []byte) ([]byte, bool) {
+	var decryptNonce [24]byte
+	copy(decryptNonce[:], encrypted[:24])
+	var keyArr [32]byte
+	copy(keyArr[:], key)
+	return secretbox.Open(nil, encrypted[24:], &decryptNonce, &keyArr)
+
+}
+
 // Wrapper around box.Seal to create a randomly generated nonce
 func BoxSeal(message []byte, pub *[32]byte, priv *[32]byte) (out []byte, err error) {
 	var nonce [24]byte
@@ -42,4 +60,57 @@ func BoxSeal(message []byte, pub *[32]byte, priv *[32]byte) (out []byte, err err
 		return nil, err
 	}
 	return box.Seal(nonce[:], message, &nonce, pub, priv), nil
+}
+
+func BoxOpen(encrypted []byte, pub *[32]byte, priv *[32]byte) ([]byte, bool) {
+	var decryptNonce [24]byte
+	copy(decryptNonce[:], encrypted[:24])
+	return box.Open(nil, encrypted[24:], &decryptNonce, pub, priv)
+}
+
+// Retrieve master private key
+func GetMasterPrivKey() (masterPrivKey [32]byte) {
+	pass, err := io.PromptPass("Please enter master password")
+	if err != nil {
+		log.Fatalf("Could not read password: %s", err.Error())
+	}
+
+	cfile, err := io.GetConfigFile()
+	if err != nil {
+		log.Fatalf("Could not get config file: %s", err.Error())
+	}
+
+	var c io.ConfigFile
+
+	configFileBytes, err := ioutil.ReadFile(cfile)
+	if err != nil {
+		log.Fatalf("Could not read config file: %s", err.Error())
+	}
+
+	err = json.Unmarshal(configFileBytes, &c)
+	if err != nil {
+		log.Fatalf("Could not read unmarshal config file: %s", err.Error())
+	}
+
+	validateMasterPassword(pass, string(c.MasterPassKey))
+
+	masterPrivKeySlice, ok := SecretboxOpen(c.MasterPassKey, c.MasterPrivKeySealed)
+
+	if !ok {
+		log.Fatalf("Failed to get master private key")
+	}
+
+	copy(masterPrivKey[:], masterPrivKeySlice)
+	fmt.Println("Authentication success!")
+	return
+}
+
+func validateMasterPassword(input string, encryptedMasterPassword string) {
+	match, err := argon2id.ComparePasswordAndHash(input, encryptedMasterPassword)
+	if err != nil {
+		log.Fatalf("Error comparing password: %s", err.Error())
+	}
+	if !match {
+		log.Fatalf("Wrong master password")
+	}
 }
