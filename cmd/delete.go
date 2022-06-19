b2a3ac32935f196a79e30cac2dfbf42b888ca6fd
Jeremy Phua <jeremyphuachengtoon@gmail.com>
2022-06-19 16:01:49 +0800

    implement show command and clean up

--- cmd/delete.go
+++ cmd/delete.go
@@ -4,11 +4,6 @@ Copyright © 2022 JEREMY PHUA <jeremyphuachengtoon@gmail.com>
 package cmd
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
-	"github.com/jeremyphua/mypass/db"
 	"github.com/spf13/cobra"
 )
 
@@ -18,13 +13,7 @@ var deleteCmd = &cobra.Command{
 	Short: "Delete credentials",
 	Long:  `Remove credentials for the specified website`,
 	Run: func(cmd *cobra.Command, args []string) {
-		site := strings.Join(args, " ")
-		err := db.DeleteCredentials(site)
-		if err != nil {
-			fmt.Println("Something went wrong:", err)
-			os.Exit(1)
-		}
-		fmt.Printf("Credentials for website %s deleted\n", site)
+
 	},
 }
 
