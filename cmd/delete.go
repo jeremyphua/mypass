09c76e4d405bfa74f45dcc66dbc7db99cf008305
Jeremy Phua <jeremyphuachengtoon@gmail.com>
2022-06-29 01:29:54 +0800

    implement delete function

--- cmd/delete.go
+++ cmd/delete.go
@@ -4,16 +4,18 @@ Copyright © 2022 JEREMY PHUA <jeremyphuachengtoon@gmail.com>
 package cmd
 
 import (
+	"github.com/jeremyphua/mypass/edit"
 	"github.com/spf13/cobra"
 )
 
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
-	Short: "Delete credentials",
-	Long:  `Remove credentials for the specified website`,
+	Short: "Remove a specific site from the vault by specifying the site-path",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-
+		siteName := args[0]
+		edit.DeleteSite(siteName)
 	},
 }
 
